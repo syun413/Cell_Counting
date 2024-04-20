584ce394f8be2b47a09d52b6eccc05bb0fb034e7
賴亮昕 <sunny990284@gmail.com>
2024-04-21 02:07:09 +0800

    augmentation - color, rotate, sharpen

--- augmentation.py
+++ augmentation.py
@@ -1,5 +1,5 @@
 """
-Pre-request: pip install pillow
+Pre-request: pip install pillow, pip install opencv-python
 Show usage: python augmentation.py -h
 """
 
@@ -7,18 +7,20 @@ Show usage: python augmentation.py -h
 import os
 import sys
 import argparse
-from PIL import Image
+from PIL import Image, ImageFilter
+import cv2
+import numpy as np
 import xml.etree.ElementTree as ET
 
 
 color_flags={1: "red", 2: "blue", 3: "green", 4: "co_rb", 5: "co_gb", 6: "co_rg"}
 flags_color={"red": 1, "blue": 2, "green": 3, "co_rb": 4, "co_gb": 5, "co_rg": 6}
 def mod_color(tif_path, xml_path, output_tif, output_xml):
-  # change color
-  image = Image.open(tif_path)
-  r, g, b = image.split()
-  output_image = Image.merge("RGB", (g, b, r))
-  output_image.save(output_tif)
+  # modify tif
+  with Image.open(tif_path) as image:
+    r, g, b = image.split()
+    output_image = Image.merge("RGB", (g, b, r))
+    output_image.save(output_tif)
   # modify xml
   xml = ET.parse(xml_path)
   root = xml.getroot()
@@ -34,10 +36,47 @@ def mod_color(tif_path, xml_path, output_tif, output_xml):
   xml.write(output_xml)
 
 def mod_rotate(tif_path, xml_path, output_tif, output_xml):
-  print()
+  # modify tif
+  with Image.open(tif_path) as image:
+    output_image = image.rotate(-90, expand=True)
+    output_image.save(output_tif)
+  # modify xml
+  xml = ET.parse(xml_path)
+  root = xml.getroot()
+  height = int(root.find('size').find('height').text)
+  for obj in root.findall('object'):
+    xmin = int(obj.find('bndbox').find('xmin').text)
+    ymin = int(obj.find('bndbox').find('ymin').text)
+    xmax = int(obj.find('bndbox').find('xmax').text)
+    ymax = int(obj.find('bndbox').find('ymax').text)
+    obj.find('bndbox').find('xmin').text = str(height - ymax)
+    obj.find('bndbox').find('ymin').text = str(xmin)
+    obj.find('bndbox').find('xmax').text = str(height - ymin)
+    obj.find('bndbox').find('ymax').text = str(xmax)
+  xml.write(output_xml)
 
-def mod_sharpen(tif_path, xml_path, output_tif, output_xml):
-  print()
+def mod_sharpen(tif_path, xml_path, output_tif, output_xml, factor):
+  # modify tif
+  with Image.open(tif_path) as image:
+    kernel_values = [
+            -1, -1, -1,
+            -1,  9, -1,
+            -1, -1, -1
+        ]
+    kernel = ImageFilter.Kernel((3,3), kernel_values, scale=1.0)
+    output_image = image.filter(kernel)
+    output_image.save(output_tif)
+  # modify xml
+  xml = ET.parse(xml_path)
+  xml.write(output_xml)
+
+def mod_blur(tif_path, xml_path, output_tif, output_xml, factor):
+  image = cv2.imread(tif_path, -1)
+  output_image = cv2.GaussianBlur(image, (3, 3), sigmaX=0)
+  cv2.imwrite(output_tif, output_image)
+  # modify xml
+  xml = ET.parse(xml_path)
+  xml.write(output_xml)
 
 def arg_color(tif_path, xml_path, output_path, file, idx):
   output_tif = os.path.join(output_path, file).replace('.tif', f"_{idx}.tif")
@@ -52,34 +91,43 @@ def arg_rot(tif_path, xml_path, output_path, file, idx):
   output_tif = os.path.join(output_path, file).replace('.tif', f"_{idx}.tif")
   output_xml = output_tif.replace('.tif', '.xml')
   mod_rotate(tif_path, xml_path, output_tif, output_xml)
-  return 1
+  output_tif2 = output_tif.replace(f"{idx}.tif", f"{idx+1}.tif")
+  output_xml2 = output_tif2.replace('.tif', '.xml')
+  mod_rotate(output_tif, output_xml, output_tif2, output_xml2)
+  output_tif3 = output_tif.replace(f"{idx}.tif", f"{idx+2}.tif")
+  output_xml3 = output_tif3.replace('.tif', '.xml')
+  mod_rotate(output_tif2, output_xml2, output_tif3, output_xml3)
+  return 3
 
-def arg_clar(tif_path, xml_path, output_path, file, idx):
+def arg_sharp(tif_path, xml_path, output_path, file, idx):
   output_tif = os.path.join(output_path, file).replace('.tif', f"_{idx}.tif")
   output_xml = output_tif.replace('.tif', '.xml')
-  mod_sharpen(tif_path, xml_path, output_tif, output_xml)
-  return 1
+  mod_sharpen(tif_path, xml_path, output_tif, output_xml, 4)
+  output_tif2 = output_tif.replace(f"{idx}.tif", f"{idx+1}.tif")
+  output_xml2 = output_tif2.replace('.tif', '.xml')
+  mod_blur(output_tif, output_xml, output_tif2, output_xml2, -2)
+  return 2
 
-def process_directory(input_path, output_path, color=False, rot=False, clar=False):
+def process_directory(input_path, output_path, color=False, rot=False, sharp=False, idx=1):
   """
-  處理指定目錄下的所有 TIF 和 XML 檔案。
+  Process all .tif under the input directory
   """
   for file in os.listdir(input_path):
     if file.endswith(".tif"):
       tif_path = os.path.join(input_path, file)
       xml_path = tif_path.replace('.tif', '.xml')
       if os.path.exists(xml_path):
-        idx = 1
         if(color):
           idx += arg_color(tif_path, xml_path, output_path, file, idx)
         if(rot):
           idx += arg_rot(tif_path, xml_path, output_path, file, idx)
-        if(clar):
-          idx += arg_clar(tif_path, xml_path, output_path, file, idx)
+        if(sharp):
+          idx += arg_sharp(tif_path, xml_path, output_path, file, idx)
       else:
         print(f"No XML file found for {tif_path}")
 
-# 主函數，調用 process_directory 並傳入目錄路徑
+## -------- main -------- ##
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Image data augmentation.")
   # Must-be argument
@@ -88,7 +136,10 @@ if __name__ == "__main__":
   # Optional argument
   parser.add_argument("--color", action="store_true", help="Exchange color channels in the image.")
   parser.add_argument("--rotate", action="store_true", help="Apply rotate operations on the image.")
-  parser.add_argument("--clarity", action="store_true", help="Adjust the image's sharpness.")
+  parser.add_argument("--sharpen", action="store_true", help="Adjust the image's sharpness.")
+  parser.add_argument("--index", type=int, help="The index that output filenames start from.")
 
   args = parser.parse_args()
-  process_directory(args.input_path, args.output_path, args.color, args.rotate, args.clarity)
+  if not args.index:
+    args.index = 1
+  process_directory(args.input_path, args.output_path, args.color, args.rotate, args.sharpen, args.index)
